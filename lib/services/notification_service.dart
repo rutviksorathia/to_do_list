da8aae4daa8813493e059b5da873b607562eee5c
SorathiaRutvik <sorathiarutvik2000>
2024-03-09 16:07:54 +0530

    feat: notification implementation

---
+++ lib/services/notification_service.dart
@@ -0,0 +1,6 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+
+class NotificationController {
+  static Future<void> onNotificationDisplayMethod(
+      ReceivedNotification receivedNotification) async {}
+}
